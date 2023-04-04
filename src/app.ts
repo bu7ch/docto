78746c2c09e151be28878beb68e47b4fc087c4e2
Rodolphe <dutchr@live.fr>
2023-04-04 02:23:46 +0200

    :tada: seed completed !!

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,18 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import  "./config/dbConfig";
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  return console.log(`[server]: Express is listening at http://localhost:${port}`);
+  return console.log(
+    `[server]: Express is listening at http://localhost:${port}`
+  );
 });
---
+++ src/config/dbConfig.ts
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose' 
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.MONGO_URL);
+  console.log('[database] Connection with MongoDB');
+}
+export default main
