a35f2d79bff0cdf5b886302a628a4305ad986bc1
Rodolphe <dutchr@live.fr>
2023-04-04 14:21:49 +0200

    :sparkles: adding userModel

---
+++ src/models/userModel.ts
@@ -0,0 +1,38 @@
+import mongoose,{  Schema } from 'mongoose'
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isDoctor: {
+    type: Boolean,
+    default: false,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  seenNotifications: {
+    type: Array,
+    default: [],
+  },
+  unseenNotifications: {
+    type: Array,
+    default: [],
+  },
+},
+{
+  timestamps: true,
+})
+const userModel = mongoose.model("users", userSchema);
+
+export default userModel;
