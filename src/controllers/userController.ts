3ccfb524257d5bb7198190b41223cc5caeda4f75
Rodolphe <dutchr@live.fr>
2023-04-05 10:39:42 +0200

    :sparkles: login + UI

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,7 +1,7 @@
-import { Router, Request, Response } from "express";
+import { Request, Response } from "express";
 import bcrypt from "bcryptjs";
 import User from "../models/userModel";
-
+import jwt from "jsonwebtoken";
 
 const register = async (req: Request, res: Response) => {
   try {
@@ -12,16 +12,35 @@ const register = async (req: Request, res: Response) => {
 
     const newUser = await new User(req.body);
     await newUser.save();
-    res.status(200).send({message:"User create successfully!", success: true});
+    res
+      .status(200)
+      .send({ message: "User create successfully!", success: true });
   } catch (error) {
     console.error(error);
   }
 };
 
- const login = async (req: Request, res: Response) => {
+const login = async (req: Request, res: Response) => {
   try {
-    
+    const user = await User.findOne({ email: req.body.email });
+    if (!user)
+      return res
+        .status(200)
+        .send({ message: "User does not exist", success: false });
+    const isMatch = await bcrypt.compare(req.body.password, user.password);
+    if (!isMatch)
+      return res
+        .status(200)
+        .send({ message: "Password is incorrect", success: false });
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    res
+      .status(200)
+      .send({ message: "Login successfull", success: true, data: token });
   } catch (error) {
-console.error(error);  }
-}
+    console.error(error);
+    res.status(500).send({ message: "Error Logging", success: false, error });
+  }
+};
 export { register, login };
