4d89763c608093f492c871cbc62872cc1db5a479
Rodolphe <dutchr@live.fr>
2023-06-13 09:58:51 +0200

    :art: layout icon + css for menu

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,47 +1,53 @@
-import { FC } from "react"
-import '../layout.css'
-import { Link } from "react-router-dom"
-export const Layout:FC<{children:any}> = ({children})=> {
-const userMenu = [
-  {
-    name: "Home",
-    path: "/",
-    icon: "",
-  },
-  {
-    name: "Appointements",
-    path: "/appointements",
-    icon: "",
-  },
-  {
-    name: "Profile",
-    path: "/profile",
-    icon: "",
-  },
-  {
-    name: "Logout",
-    path: "/logout",
-    icon: "",
-  },
-  
-]
-const menuToDisplay = userMenu
+import { FC } from "react";
+import "../layout.css";
+import { Link, useLocation } from "react-router-dom";
+export const Layout: FC<{ children: any }> = ({ children }) => {
+  const location = useLocation();
+  const userMenu = [
+    {
+      name: "Home",
+      path: "/",
+      icon: "ri-home-line",
+    },
+    {
+      name: "Appointements",
+      path: "/appointements",
+      icon: "ri-file-list-3-line",
+    },
+    {
+      name: "Apply Doctor",
+      path: "/apply-doctor",
+      icon: "ri-stethoscope-line",
+    },
+    {
+      name: "Profile",
+      path: "/profile",
+      icon: "ri-account-circle-line",
+    },
+    {
+      name: "Logout",
+      path: "/logout",
+      icon: "ri-door-open-fill",
+    },
+  ];
+  const menuToDisplay = userMenu;
 
   return (
-    <div  className="main">
+    <div className="main">
       <div className="layout">
         <div className="sidebar">
           <div className="sidebar-header">
             <h1>something</h1>
           </div>
           <div className="menu">
-            {menuToDisplay.map((menu) =>{
-              return(
-                <div className="d-flex">
+            {menuToDisplay.map((menu) => {
+              const isActive = location.pathname === menu.path
+              return (
+                <div className={`d-flex menu-item ${isActive && 'active-menu-item'}`}>
                   <i className={menu.icon}></i>
                   <Link to={menu.path}>{menu.name}</Link>
                 </div>
-              )
+              );
             })}
           </div>
         </div>
@@ -51,6 +57,5 @@ const menuToDisplay = userMenu
         </div>
       </div>
     </div>
-  )
-}
-
+  );
+};
