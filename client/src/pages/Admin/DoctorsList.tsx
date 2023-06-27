761d6872facb7759686539be828e57c87dae6505
Rodolphe <dutchr@live.fr>
2023-06-27 15:23:51 +0200

    :sparkles: Notifications for Approve Doctors w/ notification for user who Apply

--- client/src/pages/Admin/DoctorsList.tsx
+++ client/src/pages/Admin/DoctorsList.tsx
@@ -4,6 +4,8 @@ import { useDispatch } from 'react-redux'
 import { hideLoading, showLoading } from '../../redux/alertsSlice'
 import axios from 'axios'
 import { Table } from 'antd'
+import toast from 'react-hot-toast'
+import moment from 'moment'
 
 function DoctorsList() {
 const [doctors, setDoctors] = useState([])
@@ -21,6 +23,28 @@ const dispatch = useDispatch()
         setDoctors(response.data.data);
       }
     } catch (error) {
+      dispatch(hideLoading());
+    }
+  };
+  const changeDoctorStatus = async (record: { _id: any; userId: any }, status: any) => {
+    try {
+      dispatch(showLoading());
+      const resposne = await axios.post(
+        "/api/admin/change-doctor-account-status",
+        { doctorId: record._id, userId: record.userId, status: status },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      dispatch(hideLoading());
+      if (resposne.data.success) {
+        toast.success(resposne.data.message);
+        getDoctorsData();
+      }
+    } catch (error) {
+      toast.error('Error changing doctor account status');
       dispatch(hideLoading());
     }
   };
@@ -42,6 +66,7 @@ const dispatch = useDispatch()
     {
       title: "Created At",
       dataIndex: "createdAt",
+      render: (record: { createdAt: moment.MomentInput } , text: any) => moment(record.createdAt).format("DD-MM-YYYY")
     },
     {
       title: "Status",
@@ -52,8 +77,8 @@ const dispatch = useDispatch()
       dataIndex: "actions",
       render: (text: any, record: any) => (
         <div className="d-flex">
-          {record.status === "pending" && <h1 className="anchor">Approve</h1>}
-          {/* {record.status === "approved" && (<h1 className="anchor">Block</h1>)} */}
+          {record.status === "pending" && <h1 className="anchor" onClick={() => changeDoctorStatus(record, "approved")}>Approve</h1>}
+          {record.status === "approved" && (<h1 className="anchor" onClick={() => changeDoctorStatus(record, "blocked")}>Block</h1>)}
         </div>
         ),
     },
--- src/routes/adminRoute.ts
+++ src/routes/adminRoute.ts
@@ -40,4 +40,39 @@ router.get("/get-all-users", auth, async (req, res) => {
     });
   }
 });
+router.post(
+  "/change-doctor-account-status",
+  auth,
+  async (req, res) => {
+    try {
+      const { doctorId, status } = req.body;
+      const doctor = await Doctor.findByIdAndUpdate(doctorId, {
+        status,
+      });
+
+      const user = await User.findOne({ _id: doctor.userId });
+      const unseenNotifications = user.unseenNotifications;
+      unseenNotifications.push({
+        type: "new-doctor-request-changed",
+        message: `Your doctor account has been ${status}`,
+        onClickPath: "/notifications",
+      });
+      user.isDoctor = status === "approved" ? true : false;
+      await user.save();
+
+      res.status(200).send({
+        message: "Doctor status updated successfully",
+        success: true,
+        data: doctor,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({
+        message: "Error applying doctor account",
+        success: false,
+        error,
+      });
+    }
+  }
+);
 export { router as adminRoute };
